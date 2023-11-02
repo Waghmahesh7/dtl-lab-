26fb9fe27b950836c2bf25eb309ccb13f7212815
Waghmahesh7 <149069893+Waghmahesh7@users.noreply.github.com>
2023-11-02 08:13:48 +0000

    added decimal to hexadecimal converter

--- lab.c
+++ lab.c
@@ -6,7 +6,7 @@ int main() {
     double num1, num2, result;
 
     // Input the operator and operands
-    printf("Enter operator (+, -, *, /, ^): ");
+    printf("Enter operator (+, -, *, /, ^, h): ");
     scanf("%c", &operator);
 
     printf("Enter two numbers: ");
@@ -35,6 +35,16 @@ int main() {
         case '^':
             result = pow(num1, num2);
             break;
+        case 'h':
+        { 
+              int decimalNumber;
+            // Input the decimal number
+            printf("Enter a decimal number: ");
+            scanf("%d", &decimalNumber);
+            // Convert decimal to hexadecimal
+            printf("Hexadecimal equivalent: %X\n", decimalNumber);
+            break;
+        }
         default:
             printf("Error: Invalid operator.\n");
             return 1; // Exit with an error code
